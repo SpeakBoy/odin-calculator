595a5bc51ea9cafd31df150cca1bcdac25f328ce
SpeakBoy <speakman@mcmaster.ca>
2024-05-10 19:42:17 -0400

    Add divide by zero error and rounding

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let operator = "";
 let numberTwo = "";
 let result;
 let displayValue;
+const CALCULATOR_MAX_LENGTH = 12;
 const display = document.querySelector(".display");
+
 const clearBtn = document.querySelector(".clear-button");
 clearBtn.addEventListener("click", clearDisplay);
 const numberBtns = document.querySelectorAll(".number-button");
@@ -55,10 +57,19 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) {
+        return "ERROR LOL";
+    }
     return num1 / num2;
 }
 
 function operate(num1, num2, operator) {
+    if (num1 === "") {
+        num1 = 0;
+    }
+    if (num2 === "") {
+        num2 = 0;
+    }
     if (operator === "+") {
         return add(num1, num2);
     } else if (operator === "-") {
@@ -79,22 +90,33 @@ function clearDisplay() {
 }
 
 function updateDisplay(input) {
-    if (!isNaN(input)) {
-        if ((numberTwo === "" && operator !== "") || numberOne === result) {
-            display.textContent = input;
-        } else {
-            display.textContent += input;
+    if (result !== "ERROR LOL") {
+        if (!isNaN(input)) {
+            if (display.textContent.length < CALCULATOR_MAX_LENGTH) {
+                if (numberTwo === "" && operator !== "") {
+                    display.textContent = input;
+                } else {
+                    display.textContent += input;
+                }
+
+                displayValue = display.textContent;
+                return displayValue;
+            }
+        } else if (numberTwo !== "" && input === "=") {
+            numberTwo = displayValue;
+            result = String(
+                operate(parseFloat(numberOne), parseFloat(numberTwo), operator)
+            );
+            numberOne = result;
+            if (result.length > 12) {
+                result = String(parseFloat(result).toFixed(2));
+                if (result.length > 12) {
+                    result = String(parseFloat(result).toExponential(2));
+                }
+            }
+            display.textContent = result;
+            operator = "";
+            numberTwo = "";
         }
-        displayValue = display.textContent;
-        return displayValue;
-    } else if (numberTwo !== "" && input === "=") {
-        numberTwo = displayValue;
-        result = String(
-            operate(parseFloat(numberOne), parseFloat(numberTwo), operator)
-        );
-        numberOne = result;
-        display.textContent = result;
-        operator = "";
-        numberTwo = "";
     }
 }

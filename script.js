29e928b0ca0cbff080287fce43b89c33bfeb34f4
SpeakBoy <speakman@mcmaster.ca>
2024-05-10 17:42:52 -0400

    Add calculator functionality

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-let numberOne;
-let operator;
-let numberTwo;
+let numberOne = "";
+let operator = "";
+let numberTwo = "";
+let result;
 let displayValue;
 const display = document.querySelector(".display");
 const clearBtn = document.querySelector(".clear-button");
 clearBtn.addEventListener("click", clearDisplay);
 const numberBtns = document.querySelectorAll(".number-button");
 
-for (i = 0; i < numberBtns.length; i++) {
+for (let i = 0; i < numberBtns.length; i++) {
     const number = numberBtns[i].textContent;
     numberBtns[i].addEventListener("click", () => {
-        updateDisplay(number);
+        if (operator === "") {
+            numberOne = updateDisplay(number);
+        } else {
+            numberTwo = updateDisplay(number);
+        }
     });
 }
+
+const operatorBtns = document.querySelectorAll(".operator-button");
+
+for (let i = 0; i < operatorBtns.length; i++) {
+    operatorBtns[i].addEventListener("click", () => {
+        if (operator !== "") {
+            numberTwo = displayValue;
+            numberOne = String(
+                operate(parseFloat(numberOne), parseFloat(numberTwo), operator)
+            );
+            display.textContent = numberOne;
+            numberTwo = "";
+        }
+        operator = operatorBtns[i].textContent;
+        updateDisplay(operator);
+    });
+}
+
+const equalsBtn = document.querySelector(".equals-button");
+
+equalsBtn.addEventListener("click", () => {
+    updateDisplay("=");
+});
 
 function add(num1, num2) {
     return num1 + num2;
@@ -32,13 +60,13 @@ function divide(num1, num2) {
 
 function operate(num1, num2, operator) {
     if (operator === "+") {
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator === "-") {
-        subtract(num1, num2);
+        return subtract(num1, num2);
     } else if (operator === "*") {
-        multiply(num1, num2);
+        return multiply(num1, num2);
     } else if (operator === "/") {
-        divide(num1, num2);
+        return divide(num1, num2);
     }
 }
 
@@ -51,12 +79,21 @@ function clearDisplay() {
 
 function updateDisplay(input) {
     if (!isNaN(input)) {
-        display.textContent += input;
-        displayValue = display.textContent;
-        if (operator === "") {
-            numberOne = displayValue;
+        if ((numberTwo === "" && operator !== "") || numberOne === result) {
+            display.textContent = input;
         } else {
-            numberTwo = displayValue;
+            display.textContent += input;
         }
+        displayValue = display.textContent;
+        return displayValue;
+    } else if (numberTwo !== "" && input === "=") {
+        numberTwo = displayValue;
+        result = String(
+            operate(parseFloat(numberOne), parseFloat(numberTwo), operator)
+        );
+        numberOne = result;
+        display.textContent = result;
+        operator = "";
+        numberTwo = "";
     }
 }

cfc7ed6765fa34da8b0decbf2a08ed14d2cf1382
SpeakBoy <speakman@mcmaster.ca>
2024-05-10 00:24:14 -0400

    Add display update for number and clear buttons

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let numberOne;
 let operator;
 let numberTwo;
+let displayValue;
 const display = document.querySelector(".display");
+const clearBtn = document.querySelector(".clear-button");
+clearBtn.addEventListener("click", clearDisplay);
+const numberBtns = document.querySelectorAll(".number-button");
+
+for (i = 0; i < numberBtns.length; i++) {
+    const number = numberBtns[i].textContent;
+    numberBtns[i].addEventListener("click", () => {
+        updateDisplay(number);
+    });
+}
 
 function add(num1, num2) {
     return num1 + num2;
@@ -29,4 +40,13 @@ function operate(num1, num2, operator) {
     } else if (operator === "/") {
         divide(num1, num2);
     }
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+function updateDisplay(input) {
+    display.textContent += input;
+    displayValue = parseInt(display.textContent);
 }
